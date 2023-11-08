30054ecfec0134f68e4a720a8244a1841a9c550b
Eduardo <eduardogbg@gmail.com>
2023-11-08 19:05:38 -0300

    add commands enum

--- temis/src/main.rs
+++ temis/src/main.rs
@@ -1,3 +1,30 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(ValueEnum, Debug, Clone)] // ArgEnum here
+#[clap(rename_all = "kebab_case")]
+enum Command {
+    Generate,
+}
+
+impl Command {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Command::Generate => "generate",
+        }
+    }
+}
+
+#[derive(Parser, Debug, Clone)]
+struct Args {
+    command: Command,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Command::Generate => {}
+    }
+
+    println!("Hello, {}!", args.command.as_str());
 }
